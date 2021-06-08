574903bb941ed22f3d944fdcefea696203713a78
StartaBafras <beyhankaradag00@gmail.com>
2021-06-08 19:37:33 +0300

    Explanations have been edited

--- experimental.c
+++ experimental.c
@@ -16,22 +16,23 @@ int main(void)
     hints.ai_family = AF_UNSPEC;
     hints.ai_socktype = SOCK_STREAM;
 
-    char str[3] = "80";
+    char str[4] = "80";
     for(int i=0;i<200;i++)
     {
         
         sprintf(str, "%d", i);
-        
-        getaddrinfo("www.google.com", "443" , &hints, &res);
+    
+        getaddrinfo("www.google.com", str , &hints, &res);
 
         sockfd = socket(res->ai_family, res->ai_socktype, res->ai_protocol);
-
+        printf("%d portu kontrol ediliyor\n ",i);
         int c = connect(sockfd, res->ai_addr, res->ai_addrlen);
 
         if(c != -1)
         {
-            printf("\n%d",i);
+            printf("%d Port açık \n ",i);
         }
+        else printf("%d Portu Kapalı \n ",i);
         
     }
 
