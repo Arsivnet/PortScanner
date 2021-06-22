b03e42677e95acc979d27036f5b2927c49a80294
Arsivnet <arsivnet@hotmail.com>
2021-06-22 14:27:08 +0000

    multi threading try #1

---
+++ withmultithreading.c
@@ -0,0 +1,79 @@
+#include <pthread.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <stdio.h>
+#include <netdb.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define DOMAIN "www.google.com" //owner of the ports we're checking.
+#define START 60 //start range of ports
+#define END 90 //last port we're going to check
+
+typedef struct arguments{
+
+struct addrinfo *res; 
+
+int sockfd;
+
+int i;
+
+} arguments;
+
+void *ThreadingFun(void* arg){
+	
+	pthread_detach(pthread_self());
+	
+	struct arguments *par = arg;
+        
+	printf("%d portu kontrol ediliyor\n ",par->i);
+        
+	int c = connect(par->sockfd, par->res->ai_addr, par->res->ai_addrlen);
+
+        if(c != -1)
+            printf("%d Port açık \n ",par->i);
+        else 
+	printf("%d Portu Kapalı \n ",par->i);
+
+	pthread_join(pthread_self(), NULL);
+	pthread_exit(NULL);
+}
+
+int run(void){
+
+    struct addrinfo hints; //hints basically determines which sockets get to be in res.
+    struct addrinfo *res; //sockets we get from getaddrinfo, restricted by the attributes we specified in hints.
+    int sockfd;
+    pthread_t tid;
+    struct arguments Parameters;
+
+    memset(&hints, 0, sizeof hints);
+    hints.ai_family = AF_INET; //We only want IPV4 sockets in res. So we specify this in hints. Use AF_INET6 for IPV6 and AF_UNSPEC for both.
+    hints.ai_socktype = SOCK_STREAM;
+
+    char service[4] = "80"; //Internet service and it's port number. 80 is arbitrary as we're using this variable to convert i to string. But you can type the number or the name of the service you want to check here and it will return the ports associated with that service. Number 80 is for HTTP.
+    for(int i = START ;i < END ;i++)
+    {
+
+        sprintf(service, "%d", i);
+
+        getaddrinfo(DOMAIN, service , &hints, &res);
+
+	sockfd = socket(res->ai_family, res->ai_socktype, res->ai_protocol);
+	
+	Parameters.res = res;
+	Parameters.sockfd = sockfd;
+	Parameters.i = i;
+	
+        pthread_create(&tid, NULL, ThreadingFun, &Parameters);
+    }
+
+
+return 0;
+}
+
+int main(void)
+{
+run();
+return 0;
+}
